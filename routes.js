8648eee00460e0b27bedd08009db5504031b034e
emmakidney <59649705+emmakidney@users.noreply.github.com>
2021-02-25 20:37:29 +0000

    Add files via upload
    Lab02-b updates

---
+++ routes.js
@@ -0,0 +1,16 @@
+const Donations = require("./app/controllers/donations");
+
+module.exports = [
+  { method: "GET", path: "/", config: Donations.index },
+  { method: "GET", path: "/signup", config: Donations.signup },
+  { method: "GET", path: "/login", config: Donations.login },
+  {
+    method: "GET",
+    path: "/{param*}",
+    handler: {
+      directory: {
+        path: "./public",
+      },
+    },
+  },
+];
